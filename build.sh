64403700e69c0296371bfd2dcd53a7a0ae926a11
Sean Harrison <sah@bookgenesis.com>
2020-10-31 15:48:58 -0500

    don't include timestamp in docker image tag

--- build.sh
+++ build.sh
@@ -6,11 +6,11 @@ TIMESTAMP=$(date -u +%Y%m%d%H%M%S)
 SHORT_SHA=$(git rev-parse --short HEAD)
 
 docker build docker --file docker/Dockerfile --tag kruxia/svn:alpine
-docker tag kruxia/svn:alpine kruxia/svn:alpine-$TIMESTAMP-$SHORT_SHA
+docker tag kruxia/svn:alpine kruxia/svn:alpine-$SHORT_SHA
 docker tag kruxia/svn:alpine kruxia/svn
 
 # docker build docker --file docker/buster-slim.Dockerfile --tag kruxia/svn:buster-slim
-# docker tag kruxia/svn:buster-slim kruxia/svn:buster-slim-$TIMESTAMP-$SHORT_SHA
+# docker tag kruxia/svn:buster-slim kruxia/svn:buster-slim-$SHORT_SHA
 
 while test $# -gt 0
 do
@@ -18,7 +18,7 @@ do
         --push)
             docker push kruxia/svn:latest
             docker push kruxia/svn:alpine
-            docker push kruxia/svn:alpine-$TIMESTAMP-$SHORT_SHA
+            docker push kruxia/svn:alpine-$SHORT_SHA
             ;;
     esac
     shift
